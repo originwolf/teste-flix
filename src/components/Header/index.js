ae315b49b33208a4882816316ee7b5487045486f
Pedro Martins <pedrogmhead@gmail.com>
2020-09-16 09:12:24 -0300

    Added navbar

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">
+                    <img src="https://fontmeme.com/permalink/200916/fe84330af15cb3ca1090927ce75bf560.png" height="30" alt="aulaflix homepage"></img>
+                </a>
+            </nav>
+        </header>
+      );
+}
+
+export default Header;
