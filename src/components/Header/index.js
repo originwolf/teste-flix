23b57aa4bb9bce4f95b503d46872a4bcef987d98
Pedro Martins <pedrogmhead@gmail.com>
2020-10-16 21:15:08 -0300

    Changed navbar structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Logo from "../../assets/images/logo.png"
+import "./index.css"
 
 function Header() {
     return (
-        <header>
-            <nav className="navbar navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">
-                    <img src="https://fontmeme.com/permalink/200916/fe84330af15cb3ca1090927ce75bf560.png" height="30" alt="aulaflix homepage"></img>
+        <header className="container-fluid">
+            <nav className="navbar navbar-dark bg-dark row">
+                {/*eslint-disable-next-line */}
+                <a className="navbar-brand col-5 col-sm-3 col-md-2" href="#">
+                    <img className="img-fluid" src={Logo} alt="aulaflix homepage"></img>
                 </a>
             </nav>
         </header>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from "./components/Header";
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
