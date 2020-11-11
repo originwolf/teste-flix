7c8e87fd0444eda79c1badf5496646e7e604ec8e
Pedro Martins <pedrogmhead@gmail.com>
2020-11-11 09:23:44 -0300

    added required scripts

--- src/App.js
+++ src/App.js
@@ -5,28 +5,18 @@ import Carousel from "./components/Carousel";
 
 function App() {
   return (
-    <article className="banner">
+    <div className="banner">
       <Banner
         videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
         url = {dadosIniciais.categorias[0].videos[0].url}
         videoDescription = {"By Pedro Martins"}
       />
-      <div className="py-5 bg-light-gray">
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais.categorias[0]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[1]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[2]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[3]}
-        />
-      </div>
-    </article>
+      
+        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+        <Carousel category={dadosIniciais.categorias[1]} />
+        <Carousel category={dadosIniciais.categorias[2]} />
+        <Carousel category={dadosIniciais.categorias[3]} />
+    </div>
   );
 }
 
